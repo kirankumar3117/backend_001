5d7d47245b0f17f5b88420c76ef984391cda9086
kiran <ch.kirankumar311@gmail.cm>
2023-03-16 22:50:00 +0530

    PORT

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 app.use(express.json());
 app.use(cors());
 
-const Port= process.env.port || 8080 ;
+const Port= process.env.PORT || 8080 ;
 
 
 app.use("",userController);
