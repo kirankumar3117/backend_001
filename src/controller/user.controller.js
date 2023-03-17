221cc1fa136e40c80b9f85661c3892533003f56b
kiran <ch.kirankumar311@gmail.cm>
2023-03-17 15:37:29 +0530

    get-email

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -60,7 +60,7 @@ router.post("/login",async(req,res)=>{
 })
 
 router.get('/email', verifyToken, (req, res) => {
-    res.status(200).json(req.body.email);
+    res.status(200).json({req.body.email});
 });
 router.get('/protected', verifyToken, (req, res) => {
     res.status(200).json({ message: 'Protected route' });
