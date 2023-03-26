61db34ccf421333242ea5c61d72ee1b50a1460b9
kirankumar3117 <ch.kirankumar311@gmail.com>
2023-03-26 17:15:38 +0530

    limit-extens-body-parser

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express=require("express");
 const cors=require("cors");
 const app=express();
 const userController=require("./controller/user.controller");
+const bodyParser = require('body-parser');
 
 require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
-
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 const Port= process.env.PORT || 8000 ;
 
 
